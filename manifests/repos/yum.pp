a55517a4366de8aee222b31704362fa0c9c50d18
David Llorente <d.llorente@billymob.com>
2022-02-09 18:04:16 +0100

    Added descr to yumrepo

--- manifests/repos/yum.pp
+++ manifests/repos/yum.pp
@@ -11,6 +11,7 @@ class mongodb::repos::yum {
   yumrepo { 'mongodb_yum_repo':
     ensure   => present,
     name     => 'MongoDB_Repository',
+    descr    => 'MongoDB Official Yum Repository',
     baseurl  => 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/',
     enabled  => '1',
     gpgcheck => '1',
