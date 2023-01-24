2918c51939478a66c1d4543695682857bada3c51
David Llorente <d.llorente@billymob.com>
2023-01-24 17:47:12 +0100

    Yum repo URL parametrized with the version specified in package_ensure

--- manifests/repos/yum.pp
+++ manifests/repos/yum.pp
@@ -6,16 +6,25 @@
 #
 # None.
 #
-class mongodb::repos::yum {
+class mongodb::repos::yum (
+  $package_ensure = $::mongodb::package_ensure,
+) {
+
+  if (($package_ensure =~ /(\d+\.*)+\d/) and (versioncmp($package_ensure, '3.0.0') >= 0)) {
+    $mongovernumbers = split($package_ensure, '[.]')
+    $mongomajorminor = "${$mongover[0]}.${$mongover[1]}"
+  } else { # if package_ensure does not specify version, assume 5.0
+    $mongomajorminor = "5.0"
+  }
 
   yumrepo { 'mongodb_yum_repo':
     ensure   => present,
-    name     => 'MongoDB_Repository',
-    descr    => 'MongoDB Official Yum Repository',
-    baseurl  => 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/',
+    name     => "MongoDB $mongomajorminor repo",
+    descr    => "MongoDB $mongomajorminor Official Yum Repository",
+    baseurl  => "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/$mongomajorminor/x86_64/",
     enabled  => '1',
     gpgcheck => '1',
-    gpgkey   => 'https://www.mongodb.org/static/pgp/server-5.0.asc',
+    gpgkey   => "https://www.mongodb.org/static/pgp/server-$mongomajorminor.asc",
   }
 
 }
